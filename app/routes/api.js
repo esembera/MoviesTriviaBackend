a049d999caf67be6c1f20742ae68d9532e45a47b
esembera <ernest.sembera@gmail.com>
2023-04-01 22:39:47 +0200

    start working on generating quiz questions

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 module.exports = function (express, pool) {
   const apiRouter = express.Router();
@@ -7,8 +9,50 @@ module.exports = function (express, pool) {
     res.json({ message: "Dobro dosli na nas API!" });
   });
 
-  apiRouter.get("/quiz-generator/:movies", function (req, res) {
-    const { movies } = req.params;
+  async function getMovieInfo(id) {
+    const response = await fetch(
+      `${process.env.MOVIESDB_BASE_URL}/movie/${id}?api_key=${process.env.MOVIESDB_API_KEY}&language=en-US`
+    ).then((movieInfo) => {
+      return movieInfo;
+    });
+
+    const responseJson = response.json();
+
+    return responseJson;
+  }
+
+  async function getMovieCast(id) {
+    const response = await fetch(
+      `${process.env.MOVIESDB_BASE_URL}/movie/${id}/credits?api_key=${process.env.MOVIESDB_API_KEY}&language=en-US`
+    ).then((movieInfo) => {
+      return movieInfo;
+    });
+
+    const responseJson = response.json();
+    return responseJson;
+  }
+
+  apiRouter.post("/quiz-generator", function (req, res) {
+    // /quiz-generator&movies=
+    const { movies } = req.body;
+    if (movies) {
+      const movie = movies[0];
+      const movieInfo = getMovieInfo(movie);
+      movieInfo.then(function (result) {
+        console.log(result);
+      });
+      const movieCast = getMovieCast(movie);
+      movieCast.then(function (result) {
+        console.log(result?.cast[0]);
+      });
+      // movies.forEach((id) => {
+      //   const response = getMovieInfo(id);
+      //   response.then(function (result) {
+      //     console.log(result);
+      //   });
+      //   // console.log(id, response);
+      // });
+    }
     res.json({ message: `bok` });
   });
 
